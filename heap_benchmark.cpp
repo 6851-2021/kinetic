2e83bb7a5be428d7450bee4fd4fc86f405b442d9
timerstime <musicbow@163.com>
2021-05-20 21:48:39 -0400

    each step

--- heap_benchmark.cpp
+++ heap_benchmark.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    const int num_particle = 2000;
+    const int num_particle = 50000;
     std::array<int, num_particle> velocities {};
     std::array<int, num_particle> positions {};
     // MinHeap<int, int, true> heap(nullptr);
@@ -53,14 +53,14 @@ int main(int argc, char** argv) {
             heap.add(i, 0);
         std::optional heap_min = heap.min();
         clock_t heap_end= clock();
-        //std::cout << j << " [Naive heap]" << "min: "<< heap_min.value().value << " time: "<< (heap_end - heap_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
+        std::cout << j << " [Naive heap]" << "min: "<< heap_min.value() << " time: "<< (heap_end - heap_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
         heap_time_sum += (heap_end - heap_start) * 1.0 / CLOCKS_PER_SEC*1000;
         // Update the kinetic heap and the heap min with a running time
         clock_t kinetic_heap_start = clock();
         kinetic_heap.fastforward(time_inc);
         int kinetic_heap_min = kinetic_heap.min().value().value;
         clock_t kinetic_heap_end = clock();
-        //std::cout << j << " [Kinetic heap]" << "min: "<< kinetic_heap_min << " time: "<< (kinetic_heap_end - kinetic_heap_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
+        std::cout << j << " [Kinetic heap]" << "min: "<< kinetic_heap_min << " time: "<< (kinetic_heap_end - kinetic_heap_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
         kinetic_heap_time_sum += (kinetic_heap_end - kinetic_heap_start) * 1.0 / CLOCKS_PER_SEC*1000;
     }
     std::cout << "Naive heap total time:" << heap_time_sum << "ms" << std::endl;
