8225a4556b799be280957dcd4c2d3e37e2e1e11c
Erik Demaine <edemaine@mit.edu>
2021-06-06 14:27:45 -0400

    Remove old benchmark code, rename output

--- succ_benchmark.cpp
+++ succ_benchmark.cpp
@@ -14,7 +14,7 @@
 #define assert2(cond, str) if (!(cond)) {throw std::logic_error(str);}
 
 int main(int argc, char** argv) {
-    const int num_particle = 50000;
+    const int num_particle = 5000;
     int time = 0;
     std::array<int, num_particle> velocities {};
     std::array<int, num_particle> positions {};
@@ -50,39 +50,54 @@ int main(int argc, char** argv) {
         int idx = std::experimental::randint(0, (int)moving_objs.size());
         query.push_back(moving_objs[idx]);
     }
-    std::vector<MovingObject<int>> moving_obj_brute = moving_objs; // Record the particles for Brute-force method
+    KineticSuccessor kinetic_successor(moving_objs, &time); // Initialize the kinetic DS
+    // std::vector<MovingObject<int>> moving_obj_brute = moving_objs; // Record the particles for Brute-force method
     double time_sum = 0;
     double kinetic_time_sum = 0;
     // std::optional succ;
     for (int j = 0; j < time_incs.size(); j++){
         int time_inc = time_incs[j]; 
     // Update the position array for the Brute-force method
-        clock_t start = clock();
-        for (int k = 0; k < moving_obj_brute.size(); k++){
-            *(moving_obj_brute[k].curtime) = *(moving_obj_brute[k].curtime) + time_inc;
-        }
-        sort(moving_obj_brute.begin(), moving_obj_brute.end());
-        for (int k = 0; k < moving_obj_brute.size(); k++){
-            if (moving_obj_brute[k].value == query[j].value)
-            std::optional    succ = k < moving_obj_brute.size()-1 ? std::make_optional(moving_obj_brute[k+1].value) : std::nullopt; 
-        }
-        clock_t end= clock();
-        // std::cout << j << " [Naive]" << "succ: "<< succ.value << " time: "<< (end - start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
-        std::cout << j << " [Naive]" << " time: "<< (end - start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
-        time_sum += (end - start) * 1.0 / CLOCKS_PER_SEC*1000;
-        time_sum += (end - start) * 1.0 / CLOCKS_PER_SEC*1000;
+        
         // Update the kinetic heap and the heap min with a running time
-    
-        KineticSuccessor kinetic_successor(moving_objs, &time);
         clock_t kinetic_start = clock();
+        std::cout << "REE" << std::endl;
         kinetic_successor.fastforward(time_inc);
-        std::optional kinetic_succ = kinetic_successor.findSuccessor(query[j]);
+        std::cout << "dead " << std::endl;
+        // std::optional kinetic_succ = kinetic_successor.findSuccessor(query[j]);
         clock_t kinetic_end = clock();
         // std::cout << j << " [Kinetic]" << "succ: "<< kinetic_succ.value().value << " time: "<< (kinetic_end - kinetic_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
         std::cout << j << " [Kinetic]" << " time: "<< (kinetic_end - kinetic_start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
         kinetic_time_sum += (kinetic_end - kinetic_start) * 1.0 / CLOCKS_PER_SEC*1000;
+
+        clock_t start = clock();
+        // for (int k = 0; k < moving_obj_brute.size(); k++){
+        //     *(moving_obj_brute[k].curtime) = *(moving_obj_brute[k].curtime) + time_inc;
+        // }
+        std::cout << "cc"<< std::endl;
+        sort(moving_objs.begin(), moving_objs.end());
+        // for (int k = 0; k < moving_obj_brute.size(); k++){
+        //     if (moving_obj_brute[k].value == query[j].value)
+        //     std::optional    succ = k < moving_obj_brute.size()-1 ? std::make_optional(moving_obj_brute[k+1].value) : std::nullopt; 
+        // }
+        clock_t end= clock();
+        // std::cout << j << " [Naive]" << "succ: "<< succ.value << " time: "<< (end - start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
+        std::cout << j << " [Naive]" << " time: "<< (end - start) * 1.0 / CLOCKS_PER_SEC*1000 << "ms" << std::endl;
+        time_sum += (end - start) * 1.0 / CLOCKS_PER_SEC*1000;
     }
     std::cout << "Naive heap total time:" << time_sum << "ms" << std::endl;
     std::cout << "Kinetic heap total time:" << kinetic_time_sum << "ms" << std::endl;
+
+    std::cout << "Initial positions: " << std::endl;
+    for (int i = 0; i < positions.size(); i++){
+        std::cout << positions[i] << " ";
+        if (i % 10 == 0 && i != 0) std::cout << std::endl;
+    }
+
+    std::cout << "Velocities: " << std::endl;
+    for (int i = 0; i < velocities.size(); i++){
+        std::cout << velocities[i] << " ";
+        if (i % 10 == 0 && i != 0) std::cout << std::endl;
+    }
 
 }
